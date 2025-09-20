a6bcb32c4fae0eca864bca090950c45b5799106f
Nenad Marinković <nenadmarinkovic@protonmail.com>
2025-09-20 14:37:30 +0200

    try dialog height mobile fix

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -54,7 +54,7 @@ const DialogContent = React.forwardRef<
         <DialogPrimitive.Content
           ref={ref}
           className={merge(
-            'relative w-full max-w-lg max-h-[calc(100vh-4rem)] overflow-y-auto rounded-xl border bg-background p-6 shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
+            'relative w-full max-w-lg max-h-full overflow-y-auto rounded-xl border bg-background p-6 shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
             fullscreen && 'sm:max-w-[calc(100vw-8rem)]',
             className
           )}
